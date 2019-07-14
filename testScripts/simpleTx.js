5a3f2f2d2621d8dd0c8d0f4acb7755b94b1a26a1
Faye Strawn <strawnfaye@gmail.com>
2019-07-14 14:41:37 -0400

    rewrote test script to include a sleep function to wait while miner initially mines a few blocks then processes tx

--- testScripts/simpleTx.js
+++ testScripts/simpleTx.js
@@ -8,19 +8,45 @@ console.log('alice`s account balance before tx: ', aliceBefore);
 console.log('lily`s account balance before tx: ', lilyBefore);
 console.log('unlocking alice`s account');
 personal.unlockAccount(alice, 'foobar123', 10000);
-console.log('sending a tx from alice to lily');
-txn = eth.sendTransaction({
-  from: alice,
-  to: lily,
-  value: web3.toWei(1, 'szabo')
-});
-aliceAfter = eth.getBalance(alice);
-lilyAfter = eth.getBalance(lily);
-console.log('alice`s account balance after tx: ', aliceAfter);
-console.log('lily`s account balance after tx: ', lilyAfter);
-// TODO: more exact calculation with gas and stuff for pass/fail condition
-if (aliceBefore > aliceAfter && lilyBefore < lilyAfter) {
-  console.log('PASSED! Transaction successfully completed.');
-} else {
-  console.log('FAIL. Something went wrong with this transaction.');
+console.log('Timeout to let miner mine a few blocks....');
+sleep(5000);
+sendTx();
+console.log('Timeout to let miner process tx....');
+sleep(5000);
+verifyTx();
+
+function sendTx() {
+  console.log('sending a tx from alice to lily');
+  txn = eth.sendTransaction({
+    from: alice,
+    to: lily,
+    value: web3.toWei(1, 'szabo')
+  });
+}
+
+function verifyTx() {
+  aliceAfter = eth.getBalance(alice);
+  lilyAfter = eth.getBalance(lily);
+  console.log('alice`s account balance after tx: ', aliceAfter);
+  console.log('lily`s account balance after tx: ', lilyAfter);
+  // TODO: more exact calculation with gas and stuff for pass/fail condition
+  if (
+    parseInt(aliceBefore, 10) > parseInt(aliceAfter, 10) &&
+    parseInt(lilyBefore, 10) < parseInt(lilyAfter, 10)
+  ) {
+    console.log('PASSED! Transaction successfully completed.');
+  } else {
+    console.log('FAIL. Something went wrong with this transaction.');
+  }
+}
+
+// Custom function to allow thread to wait for a given number of milliseconds
+// before executing the next line of code.
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
 }
